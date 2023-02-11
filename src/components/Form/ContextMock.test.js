53fee660f5794408de81ec9d340f2780a5864a8c
Vinicius Arcanjo <vsimonton2010@gmail>
2023-02-11 15:15:39 -0300

    mockando o contexto da aplicação nos testes

---
+++ src/components/Form/ContextMock.test.js
@@ -0,0 +1,76 @@
+import { render, screen } from '@testing-library/react';
+import FormProvider from '../../context/FormContext/FormContext';
+
+import { Form } from './Form';
+
+let mockParticipantsList = [];
+let mockError = '';
+const mockAddParticipants = jest.fn();
+
+const mockUseNavigate = jest.fn();
+
+jest.mock('../../context/FormContext/FormContextBuilder', () => {
+  return {
+    FormContextBuilder: () => ({
+      participantsList: mockParticipantsList,
+      error: mockError,
+      addParticipants: () => mockAddParticipants,
+    }),
+  };
+});
+
+jest.mock('react-router-dom', () => {
+  return {
+    useNavigate: () => mockUseNavigate,
+  };
+});
+
+describe('<Form /> in context', () => {
+  it('deve iniciar o botão de adicionar participantes como desabilitado', () => {
+    render(
+      <FormProvider>
+        <Form />
+      </FormProvider>,
+    );
+
+    const btns = screen.getAllByRole('button');
+
+    expect(btns[0]).toBeDisabled();
+  });
+
+  it('deve estar com o botão de iniciar brinacdeira habilitado', () => {
+    mockParticipantsList = ['Vinicius', 'Igor', 'Luciano'];
+
+    render(
+      <FormProvider>
+        <Form />
+      </FormProvider>,
+    );
+
+    const btns = screen.getAllByRole('button');
+
+    expect(btns[1]).not.toBeDisabled();
+  });
+
+  it('deve iniciar sem erro na tela', () => {
+    render(
+      <FormProvider>
+        <Form />
+      </FormProvider>,
+    );
+
+    expect(screen.queryByText('Isso é um erro')).toBeNull;
+  });
+
+  it('deve identificar o erro na tela', () => {
+    mockError = ['Isso é um erro'];
+
+    render(
+      <FormProvider>
+        <Form />
+      </FormProvider>,
+    );
+
+    expect(screen.getByText('Isso é um erro')).toBeInTheDocument;
+  });
+});
